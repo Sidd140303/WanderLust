f2f8d273f0a00e7e8f4fbbcd95b889683bc96631
Siddharth Gupta <siddharth140303@gmail.com>
2025-05-13 14:14:52 +0530

    Sample listing done

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-
-app.listen(8080, () => {
-    console.log("Server is listening...");
-
-})
+const listing = require("./models/listing.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -20,7 +16,26 @@ async function main() {
     await mongoose.connect(MONGO_URL)
 }
 
+app.get("/testListing", async (req, res) => {
+    let sampleListing = new listing({
+        title: "My new Villa",
+        description: "By the beach",
+        price: 1200,
+        location: "Goa",
+        country: "India"
+    })
+    await sampleListing.save();
+    console.log("response saved");
+    res.send("Successful testing");
+
+})
+
+
 app.get("/", (req, res) => {
     res.send("Started...");
 })
 
+app.listen(8080, () => {
+    console.log("Server is listening...");
+
+})
--- models/listing.js
+++ models/listing.js
@@ -2,9 +2,18 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
-    title: String,
+    title: {
+        type: String,
+        required: true
+    },
     description: String,
-    image: String,
+    image: {
+        type: String,
+        default: "https://images.unsplash.com/photo-1678895223308-da40c609e39b?q=80&w=1976&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        set: (v) => v === ""
+            ? "https://images.unsplash.com/photo-1678895223308-da40c609e39b?q=80&w=1976&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            : v
+    },
     price: Number,
     location: String,
     country: String
@@ -12,4 +21,4 @@ const listingSchema = new Schema({
 
 const listing = mongoose.model("listing", listingSchema);
 
-modules.export = listing;
+module.exports = listing;
